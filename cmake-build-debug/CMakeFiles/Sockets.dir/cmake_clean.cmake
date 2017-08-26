0153517cd5ce7c5ee47a360a3d63ce65ea7f4372
3bhady <mohamed_abdelhady96@hotmail.com>
2017-08-26 07:34:07 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Sockets.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sockets.dir/main.cpp.o"
+  "Sockets.pdb"
+  "Sockets"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sockets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
